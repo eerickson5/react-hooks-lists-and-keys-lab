4d413dd5b155e8f6f7e8a08adaa09fbfa06f1261
Elise Erickson <elisece717@gmail.com>
2023-12-21 19:25:19 -0500

    satisfies tests

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,13 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const LinkComponents = links.map((link) => {
+    return <a href={`#${link}`} key={link}>{link}</a>
+  })
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>
+    {LinkComponents}
+  </nav>;
 }
 
 export default NavBar;
